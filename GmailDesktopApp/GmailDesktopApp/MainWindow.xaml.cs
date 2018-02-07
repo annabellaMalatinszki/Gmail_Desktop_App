ebe164f1663b2dfb71311f66c4e5df5b5f824e42
annabellaMalatinszki <a.malatinszki@gmail.com>
2018-02-07 08:16:16 +0100

    Create connection to Gmail API

--- GmailDesktopApp/GmailDesktopApp/MainWindow.xaml.cs
+++ GmailDesktopApp/GmailDesktopApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GmailDesktopAppBackend;
 
 namespace GmailDesktopApp
 {
@@ -23,6 +24,7 @@ namespace GmailDesktopApp
         public MainWindow()
         {
             InitializeComponent();
+            MailService mailService = new MailService();
         }
     }
 }
--- GmailDesktopApp/GmailDesktopAppBackend/MailService.cs
+++ GmailDesktopApp/GmailDesktopAppBackend/MailService.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GmailDesktopAppBackend
 {
-    class MailService
+    public class MailService
     {
+        public MailService()
+        { }
     }
 }
